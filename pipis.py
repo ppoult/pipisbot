02e359d740f20db2b2583c948c2281587e24c9fd
“ppoult” <theashrayheart1996@gmail.com>
2019-10-14 13:11:18 +0300

    code's been commented

--- pipis.py
+++ pipis.py
@@ -5,7 +5,7 @@ import re
 import json
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                     level=logging.INFO) #enable logging
 
 def GetShit(): #сгребаем последнюю запись со стены Pipis
 
@@ -21,14 +21,14 @@ def GetShit(): #сгребаем последнюю запись со стены
     vk = vk_session.get_api()
 
     response = vk.wall.get(domain='pipis_everyday', count=1)  # Используем метод wall.get
-    response_two = response['items'][0]['text']
-    response_three = response['items'][0]['attachments'][0]['photo']['sizes'][7]
-    return response_two
+    response_two = response['items'][0]['text'] #parsing the JSON output for the text block
+    response_three = response['items'][0]['attachments'][0]['photo']['sizes'][7] #parsing the JSON output for the picture
+    return response_two #return the variable with the text
     return response_three
 
 def GetPic(): #сгребаем картинку со стены Pipis
 
-    login, password = 'flokinokinihilipilifications@mail.ru', 'WSK741qaz'
+    login, password = 'flokinokinihilipilifications@mail.ru', 'WSK741qaz' #authenticate
     vk_session = vk_api.VkApi(login, password)
 
     try:
@@ -39,16 +39,16 @@ def GetPic(): #сгребаем картинку со стены Pipis
 
     vk = vk_session.get_api()
 
-    response = vk.wall.get(domain='pipis_everyday', count=1)  # Используем метод wall.get
-    response_three = response['items'][0]['attachments'][0]['photo']['sizes'][7]['url']
-    return response_three
+    response = vk.wall.get(domain='pipis_everyday', count=1)  # Используем метод wall.get с VK.python
+    response_three = response['items'][0]['attachments'][0]['photo']['sizes'][7]['url'] #parsing the JSON output for the text block
+    return response_three #return the variable with the pic url
 
 
 
-def start(update, context):
+def start(update, context):  #greetings function
     context.bot.send_message(chat_id=update.effective_chat.id, text='Привет. Я — Пипис, полиморфный цыпленок. Могу подскзать, какой сегодня день.')
 
-def ask(update, context):
+def ask(update, context): #picture of the day function. Get all the return variables and paste them into the reply
     answer = GetShit()
     pic = GetPic()
     context.bot.send_message(chat_id=update.effective_chat.id, text='Бакаа, бкааа...бкаа, бка')
